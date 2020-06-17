2a429962bcdaf3918dd16adbbde49549d1f01e75
Liem Nguyen <liemnguyen0111@gmail.com>
2020-06-16 17:25:16 -0700

    Added db.js, service-worker.js, manifest.json, updated style.css with media query, index.html

---
+++ public/service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open('list-cache-v1')
+      .then(cache => cache.addAll([
+        '/',
+        '/db.js',
+        '/index.js',
+        '/styles.css',
+        '/manifest.json',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png'
+      ])))
+  })
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(fetch(event.request)
+      .catch(err => {
+        return caches.match(event.request)
+          .then(match => {
+            if (match) {
+              return match
+            } else if (event.request.headers.get('accept').includes('text/html')) {
+              return caches.match('/')
+            }
+          })
+      }))
+  })
